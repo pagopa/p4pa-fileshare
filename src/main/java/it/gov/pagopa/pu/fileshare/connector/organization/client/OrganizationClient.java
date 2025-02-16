5a6d51ce422ee7580d7ad5464c70ae274fd0121f
antonio.torre <antonio.torre@pagopa.it>
2025-02-16 15:32:27 +0100

    feat: Performance logging

--- src/main/java/it/gov/pagopa/pu/fileshare/connector/organization/client/OrganizationClient.java
+++ src/main/java/it/gov/pagopa/pu/fileshare/connector/organization/client/OrganizationClient.java
@@ -3,7 +3,6 @@ package it.gov.pagopa.pu.fileshare.connector.organization.client;
 import it.gov.pagopa.pu.fileshare.connector.organization.config.OrganizationApisHolder;
 import it.gov.pagopa.pu.p4paorganization.dto.generated.Organization;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 
@@ -17,19 +16,12 @@ public class OrganizationClient {
   }
 
   public Organization getOrganizationById(Long organizationId, String accessToken) {
-    try{
-      return organizationApisHolder.getOrganizationEntityControllerApi(accessToken).crudGetOrganization(String.valueOf(organizationId));
-
-    } catch (HttpClientErrorException e) {
-      if (e.getStatusCode() == HttpStatus.NOT_FOUND) {
-        log.warn("Organization with organization id {} not found", organizationId);
-        return null;
-      }
-      log.error("Error retrieving organization by organization id: {}", organizationId, e);
-      throw e;
-    } catch (Exception e) {
-      log.error("Unexpected error while retrieving organization by organization id: {}", organizationId, e);
-      throw e;
+    try {
+      return organizationApisHolder.getOrganizationEntityControllerApi(accessToken)
+        .crudGetOrganization(String.valueOf(organizationId));
+    } catch (HttpClientErrorException.NotFound e) {
+      log.info("Organization with organization id {} not found", organizationId);
+      return null;
     }
   }
 }
