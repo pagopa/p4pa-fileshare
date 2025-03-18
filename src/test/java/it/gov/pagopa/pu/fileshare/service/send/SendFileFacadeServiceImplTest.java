b2b163ef63771a2cb460f4f432ccf0432aa4dec2
antoniocalo <antonio.calo@nttdata.com>
2025-03-18 17:27:12 +0100

    P4ADEV-2337 fix SendFileFacadeServiceImplTest

--- src/test/java/it/gov/pagopa/pu/fileshare/service/send/SendFileFacadeServiceImplTest.java
+++ src/test/java/it/gov/pagopa/pu/fileshare/service/send/SendFileFacadeServiceImplTest.java
@@ -8,6 +8,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import it.gov.pagopa.pu.fileshare.connector.send_notification.NotificationService;
+import it.gov.pagopa.pu.fileshare.dto.SaveFileResultDTO;
 import it.gov.pagopa.pu.fileshare.exception.custom.FileUploadException;
 import it.gov.pagopa.pu.fileshare.exception.custom.InvalidFileException;
 import it.gov.pagopa.pu.fileshare.service.FileService;
@@ -16,19 +17,14 @@ import it.gov.pagopa.pu.fileshare.service.UserAuthorizationService;
 import it.gov.pagopa.pu.p4paauth.dto.generated.UserInfo;
 import it.gov.pagopa.pu.sendnotification.dto.generated.LoadFileRequest;
 import it.gov.pagopa.pu.sendnotification.dto.generated.StartNotificationResponse;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Base64;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
@@ -61,7 +57,8 @@ class SendFileFacadeServiceImplTest {
   private static final String FILE_NAME = "test.txt";
   private static final String ACCESS_TOKEN = "token123";
   private static final String VALID_DIGEST = "9e9LsYp4qQ4bjyGI4Mp/jmBN2jKehKTTaonMr1AJEPU=";
-
+  private static final String FILE_CONTENT = "TEST FILE HASH P4PA SEND";
+  private static final String FINAL_FILE_NAME = SEND_NOTIFICATION_ID + "_" + FILE_NAME;
   @BeforeEach
   void setUp() {
     ReflectionTestUtils.setField(sendFileFacadeService, "sendFolder", SEND_FOLDER);
@@ -71,28 +68,30 @@ class SendFileFacadeServiceImplTest {
   @Test
   void givenValidRequestWhenUploadSendFileThenSuccess() throws IOException {
     // Given
-    String expectedFileName = SEND_NOTIFICATION_ID + "_" + FILE_NAME;
     StartNotificationResponse expectedResponse = new StartNotificationResponse();
-    InputStream mockInputStream = new ByteArrayInputStream("TEST FILE HASH P4PA SEND".getBytes());
+    byte[] expectedFileHash = FILE_CONTENT.getBytes(); // This should match the valid hash
+    SaveFileResultDTO saveFileResult = new SaveFileResultDTO("/organization", expectedFileHash);
 
     // When
     when(multipartFile.getOriginalFilename()).thenReturn(FILE_NAME);
-    when(multipartFile.getInputStream()).thenReturn(mockInputStream);
-    when(fileStorerService.buildOrganizationBasePath(ORGANIZATION_ID))
-      .thenReturn(Path.of("org", ORGANIZATION_ID.toString()));
+    when(fileStorerService.saveToSharedFolder(ORGANIZATION_ID, multipartFile, SEND_FOLDER, FINAL_FILE_NAME))
+      .thenReturn(saveFileResult);
 
+    String validDigest = Base64.getEncoder().encodeToString(expectedFileHash);
+
+    // The valid digest used in the uploadSendFile method call
     when(notificationService.startNotification(eq(SEND_NOTIFICATION_ID), eq(ORGANIZATION_ID), any(
       LoadFileRequest.class), eq(ACCESS_TOKEN)))
       .thenReturn(expectedResponse);
 
     StartNotificationResponse result = sendFileFacadeService.uploadSendFile(
-      ORGANIZATION_ID, SEND_NOTIFICATION_ID, VALID_DIGEST, multipartFile, userInfo, ACCESS_TOKEN
+      ORGANIZATION_ID, SEND_NOTIFICATION_ID, validDigest, multipartFile, userInfo, ACCESS_TOKEN
     );
 
     // Then
     verify(userAuthorizationService).checkUserAuthorization(ORGANIZATION_ID, userInfo, ACCESS_TOKEN);
     verify(fileService).validateFile(multipartFile);
-    verify(fileStorerService).saveToSharedFolder(ORGANIZATION_ID, multipartFile, SEND_FOLDER, expectedFileName);
+    verify(fileStorerService).saveToSharedFolder(ORGANIZATION_ID, multipartFile, SEND_FOLDER, FINAL_FILE_NAME);
     assertEquals(expectedResponse, result);
   }
 
@@ -100,67 +99,87 @@ class SendFileFacadeServiceImplTest {
   void givenAlreadyUploadedWhenUploadSendFileThenSuccess() {
     MockMultipartFile file = new MockMultipartFile(
       "sendFile",
-      "orginalFileName.txt",
+      FILE_NAME,
       MediaType.TEXT_PLAIN_VALUE,
-      "TEST FILE HASH P4PA SEND".getBytes()
+      FILE_CONTENT.getBytes()
     );
 
-    Path organizationBasePath = Path.of("/organizationFolder");
     StartNotificationResponse expectedResponse = new StartNotificationResponse();
 
-    // When
-    Mockito.when(fileStorerService.buildOrganizationBasePath(ORGANIZATION_ID))
-      .thenReturn(organizationBasePath);
-    try (MockedStatic<Files> filesMockedStatic = Mockito.mockStatic(Files.class)) {
-      filesMockedStatic.when(() -> Files.exists(
-          organizationBasePath
-            .resolve(SEND_FOLDER)
-            .resolve(FILE_NAME + ".cipher")))
-        .thenReturn(true);
+    when(fileStorerService.checkIfAlreadyUploadedOrArchived(
+      ORGANIZATION_ID, ARCHIVED_SUBFOLDER, SEND_FOLDER, FINAL_FILE_NAME))
+      .thenReturn(true);
 
-      when(notificationService.startNotification(eq(SEND_NOTIFICATION_ID), eq(ORGANIZATION_ID), any(
-        LoadFileRequest.class), eq(ACCESS_TOKEN)))
-        .thenReturn(expectedResponse);
+    when(notificationService.startNotification(eq(SEND_NOTIFICATION_ID), eq(ORGANIZATION_ID), any(
+      LoadFileRequest.class), eq(ACCESS_TOKEN)))
+      .thenReturn(expectedResponse);
 
-      StartNotificationResponse result = sendFileFacadeService.uploadSendFile(
-        ORGANIZATION_ID, SEND_NOTIFICATION_ID, VALID_DIGEST, file, userInfo, ACCESS_TOKEN
-      );
+    StartNotificationResponse result = sendFileFacadeService.uploadSendFile(
+      ORGANIZATION_ID, SEND_NOTIFICATION_ID, VALID_DIGEST, file, userInfo, ACCESS_TOKEN
+    );
 
-      verify(userAuthorizationService).checkUserAuthorization(ORGANIZATION_ID, userInfo, ACCESS_TOKEN);
-      verify(fileService).validateFile(file);
-      verify(fileStorerService, never()).saveToSharedFolder(ORGANIZATION_ID, file, SEND_FOLDER, "orginalFileName.txt");
-      assertEquals(expectedResponse, result);
-    }
+    verify(userAuthorizationService).checkUserAuthorization(ORGANIZATION_ID, userInfo, ACCESS_TOKEN);
+    verify(fileService).validateFile(file);
+    verify(fileStorerService, never()).saveToSharedFolder(ORGANIZATION_ID, file, SEND_FOLDER, FINAL_FILE_NAME);
+    assertEquals(expectedResponse, result);
+
   }
 
   @Test
-  void givenInvalidFileDigestWhenUploadSendFileThenInvalidDigest()
-    throws IOException {
+  void givenInvalidFileDigestWhenUploadSendFileThenInvalidDigest() {
     // GIVEN
-    String content = "TEST FILE HASH P4PA SEND";
-    InputStream inputStream = new ByteArrayInputStream(content.getBytes(
-      StandardCharsets.UTF_8));
+    MockMultipartFile file = new MockMultipartFile(
+      "sendFile",
+      FILE_NAME,
+      MediaType.TEXT_PLAIN_VALUE,
+      FILE_CONTENT.getBytes()
+    );
+
+    SaveFileResultDTO saveFileResultDTO = new SaveFileResultDTO();
+    saveFileResultDTO.setFileHash("wrongHash".getBytes());
+    saveFileResultDTO.setRelativePath("path");
+
     // WHEN
-    when(multipartFile.getInputStream()).thenReturn(inputStream);
+    when(fileStorerService.checkIfAlreadyUploadedOrArchived(
+      ORGANIZATION_ID, ARCHIVED_SUBFOLDER, SEND_FOLDER, FINAL_FILE_NAME))
+      .thenReturn(false);
+
+    when(fileStorerService.saveToSharedFolder(
+      ORGANIZATION_ID, file, SEND_FOLDER, FINAL_FILE_NAME))
+      .thenReturn(saveFileResultDTO);
+
+    when(fileStorerService.buildOrganizationBasePath(ORGANIZATION_ID))
+      .thenReturn(Paths.get("basePath"));
 
     // THEN
     assertThrows(InvalidFileException.class, () ->
       sendFileFacadeService.uploadSendFile(
-        ORGANIZATION_ID, SEND_NOTIFICATION_ID, "WRONGDIGEST", multipartFile,
+        ORGANIZATION_ID, SEND_NOTIFICATION_ID, VALID_DIGEST, file,
         userInfo, ACCESS_TOKEN
       )
     );
   }
 
   @Test
-  void givenInvalidFileWhenUploadSendFileThenIOException() throws IOException {
-    // Given
-    when(multipartFile.getInputStream()).thenThrow(new IOException("Test IO Exception"));
+  void givenInvalidFileWhenUploadSendFileThenFileUploadException() {
+    // When
+    when(multipartFile.getOriginalFilename()).thenReturn(FILE_NAME);
+    when(fileStorerService.checkIfAlreadyUploadedOrArchived(
+      ORGANIZATION_ID, ARCHIVED_SUBFOLDER, SEND_FOLDER, FINAL_FILE_NAME))
+      .thenReturn(false);
+    when(fileStorerService.saveToSharedFolder(
+      ORGANIZATION_ID, multipartFile, SEND_FOLDER, FINAL_FILE_NAME))
+      .thenThrow(new FileUploadException("Error saving file"));
 
-    // Then
+    // Act & Assert
     assertThrows(FileUploadException.class, () ->
       sendFileFacadeService.uploadSendFile(
-        ORGANIZATION_ID, SEND_NOTIFICATION_ID, VALID_DIGEST, multipartFile, userInfo, ACCESS_TOKEN
+        ORGANIZATION_ID,
+        SEND_NOTIFICATION_ID,
+        VALID_DIGEST,
+        multipartFile,
+        userInfo,
+        ACCESS_TOKEN
       )
     );
   }
