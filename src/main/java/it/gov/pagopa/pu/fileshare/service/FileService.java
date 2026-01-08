fca17e24bdd9b0932776bb7ae1e28815967d76f4
macacia <marco.cacia@eng.it>
2026-01-08 10:47:09 +0100

    add validation on service

--- src/main/java/it/gov/pagopa/pu/fileshare/service/FileService.java
+++ src/main/java/it/gov/pagopa/pu/fileshare/service/FileService.java
@@ -47,6 +47,12 @@ public class FileService {
 
   }
 
+  public MultipartFile getExclusivePresenceOrThrow(MultipartFile a, MultipartFile b) {
+    if (a != null && b == null) return a;
+    if (a == null && b != null) return b;
+    throw new InvalidFileException("Exactly one of the two files must be non-null");
+  }
+
   private String replaceCharVersion(String fileVersion, String target, String replacement) {
     return fileVersion.replace(target, replacement);
   }
