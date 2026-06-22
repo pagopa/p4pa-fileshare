6882ea275ded67e19cd411ab0b3665e77b63b16f
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2026-06-22 17:26:51 +0200

    fix: Fix CVE (#126)

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,15 +8,15 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
   java
-  id("org.springframework.boot") version "4.0.6"
+  id("org.springframework.boot") version "4.1.0"
   id("io.spring.dependency-management") version "1.1.7"
   jacoco
-  id("org.sonarqube") version "7.2.3.7755"
+  id("org.sonarqube") version "7.3.1.8318"
   id("com.github.ben-manes.versions") version "0.54.0"
-  id("org.openapi.generator") version "7.21.0"
+  id("org.openapi.generator") version "7.23.0"
   id("org.ajoberstar.grgit") version "5.3.2"
-  id("com.gorylenko.gradle-git-properties") version "2.5.7"
-  id("com.github.jk1.dependency-license-report") version "3.1.2"
+  id("com.gorylenko.gradle-git-properties") version "4.0.1"
+  id("com.github.jk1.dependency-license-report") version "3.1.4"
 }
 
 group = "it.gov.pagopa.payhub"
@@ -53,11 +53,12 @@ repositories {
 }
 
 val springDocOpenApiVersion = "3.0.3"
-val janinoVersion = "3.1.12"
 val openApiToolsVersion = "0.2.10"
-val micrometerVersion = "1.6.5"
+val micrometerVersion = "1.7.0"
 val httpClientVersion = "5.6.1"
 val httpCoreVersion = "5.4.2"
+val kafkaAppender = "0.2.0-RC2"
+val lz4JavaVersion = "1.11.0"
 val commonsLang3Version = "3.20.0"
 
 dependencies {
@@ -74,10 +75,13 @@ dependencies {
     exclude(group = "org.apache.commons", module = "commons-lang3")
   }
   implementation("org.apache.commons:commons-lang3:$commonsLang3Version")
-  implementation("org.codehaus.janino:janino:$janinoVersion")
   implementation("org.openapitools:jackson-databind-nullable:$openApiToolsVersion")
   implementation("org.apache.httpcomponents.client5:httpclient5:$httpClientVersion")
   implementation("org.apache.httpcomponents.core5:httpcore5:$httpCoreVersion")
+  implementation("com.github.danielwegener:logback-kafka-appender:$kafkaAppender") {
+    exclude(group = "org.lz4", module = "lz4-java")
+  }
+  implementation("at.yawk.lz4:lz4-java:$lz4JavaVersion")
 
   compileOnly("org.projectlombok:lombok")
   annotationProcessor("org.projectlombok:lombok")
@@ -132,6 +136,7 @@ tasks {
       expand(projectInfo)
     }
   }
+  processResources.dependsOn("dependenciesBuild")
 }
 
 tasks.compileJava {
@@ -183,7 +188,8 @@ tasks.register<org.openapitools.generator.gradle.plugin.tasks.GenerateTask>("ope
     mapOf(
       "dateLibrary" to "java8",
       "requestMappingMode" to "api_interface",
-      "useSpringBoot3" to "true",
+      "useSpringBoot4" to "true",
+      "useJackson3" to "true",
       "interfaceOnly" to "true",
       "useTags" to "true",
       "useBeanValidation" to "true",
@@ -221,7 +227,8 @@ tasks.register<org.openapitools.generator.gradle.plugin.tasks.GenerateTask>("ope
       "openApiNullable" to "false",
       "dateLibrary" to "java8",
       "serializableModel" to "true",
-      "useSpringBoot3" to "true",
+      "useSpringBoot4" to "true",
+      "useJackson3" to "true",
       "useJakartaEe" to "true",
       "useOneOfInterfaces" to "true",
       "useBeanValidation" to "true",
@@ -251,7 +258,8 @@ tasks.register<org.openapitools.generator.gradle.plugin.tasks.GenerateTask>("ope
       "openApiNullable" to "false",
       "dateLibrary" to "java8",
       "serializableModel" to "true",
-      "useSpringBoot3" to "true",
+      "useSpringBoot4" to "true",
+      "useJackson3" to "true",
       "useJakartaEe" to "true",
       "useOneOfInterfaces" to "true",
       "useBeanValidation" to "true",
@@ -286,7 +294,8 @@ tasks.register<org.openapitools.generator.gradle.plugin.tasks.GenerateTask>("ope
       "openApiNullable" to "false",
       "dateLibrary" to "java8",
       "serializableModel" to "true",
-      "useSpringBoot3" to "true",
+      "useSpringBoot4" to "true",
+      "useJackson3" to "true",
       "useJakartaEe" to "true",
       "useBeanValidation" to "true",
       "serializationLibrary" to "jackson",
@@ -315,7 +324,8 @@ tasks.register<org.openapitools.generator.gradle.plugin.tasks.GenerateTask>("ope
       "openApiNullable" to "false",
       "dateLibrary" to "java8",
       "serializableModel" to "true",
-      "useSpringBoot3" to "true",
+      "useSpringBoot4" to "true",
+      "useJackson3" to "true",
       "useJakartaEe" to "true",
       "useOneOfInterfaces" to "true",
       "useBeanValidation" to "true",
@@ -352,7 +362,8 @@ tasks.register<GenerateTask>("openApiGeneratePAGOPAPAYMENTS") {
       "openApiNullable" to "false",
       "dateLibrary" to "java8",
       "serializableModel" to "true",
-      "useSpringBoot3" to "true",
+      "useSpringBoot4" to "true",
+      "useJackson3" to "true",
       "useJakartaEe" to "true",
       "useOneOfInterfaces" to "true",
       "useBeanValidation" to "true",
@@ -387,7 +398,8 @@ tasks.register<org.openapitools.generator.gradle.plugin.tasks.GenerateTask>("ope
       "openApiNullable" to "false",
       "dateLibrary" to "java8",
       "serializableModel" to "true",
-      "useSpringBoot3" to "true",
+      "useSpringBoot4" to "true",
+      "useJackson3" to "true",
       "useJakartaEe" to "true",
       "useOneOfInterfaces" to "true",
       "useBeanValidation" to "true",
